591936a90c1b9ce32479031fadd24d9ff723664d
sana-haouala <sana.haou95@gmail.com>
2018-10-24 21:30:55 +0100

    First Commit

---
+++ App1/App1/App1/Page1.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Page1 : ContentPage
+	{
+        List<string> list = new List<string>
+        {
+            "Hosni","Aya","Mariem" ,"Houssem","Siwar","Ala","karim","imen"
+        };
+
+		public Page1 ()
+		{
+            list.Add("oussema");
+			InitializeComponent ();
+            myLabel.Text = "Hello Xamarin Via " + Device.RuntimePlatform;
+            myList.ItemsSource = list;
+		}
+        protected async void btnClicked(object o,EventArgs e)
+        {
+            int age = 20;
+            //DisplayAlert("Message", "hello" + Device.RuntimePlatform, "ok!");  
+           await Navigation.PushAsync(new Page2());
+
+        }
+        protected void SearchPressed(object o,EventArgs e)
+        {
+           
+            myList.ItemsSource = list.Where(y=> y.Contains(mySearchBar.Text));
+        }
+    }
+}
